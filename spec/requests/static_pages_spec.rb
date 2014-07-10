2a24f63d38fc0b41614869b3dcb2ad936694981c
Paige Hubbell <paigetech@gmail.com>
2014-07-09 20:40:54 -0400

    pre chapter 6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,17 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+describe ApplicationHelper do
+  describe "full_title" do
+    it "should include the page title" do
+      expect(full_title("foo")).to match(/foo/)
+    end
 
-  describe "Home page" do
-    before { visit root_path }
-    subject { page }
+    it "should include the base title" do
+      expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
+    end
 
-      it { should have_content('Sample App') }
-
-      it { should have_title(full_title('')) }
-
-      it { should_not have_title('| Home') }
-  end
-
-  describe "Help page" do
-    before { visit help_path }
-    subject { page }
-
-      it { should have_content('Help') }
-      it { should have_title("#{base_title} | Help") }
-  end
-
-  describe "About page" do
-    before { visit about_path }
-    subject { page }
-
-      it { should have_content('About') }
-      it { should have_title("#{base_title} | About Us") }
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-    subject { page }
-
-      it { should have_content('Contact') }
-      it { should have_title("#{base_title} | Contact") }
+    it "should not include a bar for the home page" do
+      expect(full_title("")).not_to match(/\|/)
+    end
   end
 end
