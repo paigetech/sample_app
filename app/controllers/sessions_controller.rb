f758a1a14e0021bbc36953679254d0ce3c1c7806
Paige Hubbell <paigetech@gmail.com>
2014-07-16 13:54:39 -0700

    sign up

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/pass combo'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_rememebr_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
+
+
+end
